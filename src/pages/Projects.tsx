8734b4ce29d3b1950be38f5f73016cbb9b3c0355
GoTether <123700519+GoTether@users.noreply.github.com>
2025-09-30 08:03:14 -0500

    Update Projects header to include '(test)'

--- src/pages/Projects.tsx
+++ src/pages/Projects.tsx
@@ -1,7 +1,7 @@
 export default function Projects() {
   return (
     <div className="py-8">
-      <h1 className="text-3xl font-bold text-neutral-800">Projects</h1>
+      <h1 className="text-3xl font-bold text-neutral-800">Projects (test)</h1>
     </div>
   )
 }
