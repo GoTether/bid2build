9c69bc9f2c39afd71d3e379375b448a35401fa0b
GoTether <123700519+GoTether@users.noreply.github.com>
2025-10-15 16:50:44 -0500

    Set basename for BrowserRouter in main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -6,7 +6,7 @@ import App from './App.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="/bid2build">
       <App />
     </BrowserRouter>
   </StrictMode>,
